e71cd2d99a43f8cd44993e769fae02e1b3776242
Alexey Piskunov <leshatkd@mail.ru>
2017-05-04 12:40:45 +0300

    refactoring

--- app/js/common.js
+++ app/js/common.js
@@ -1,101 +1,108 @@
 $(function() {
 	const mainContent = $(".main-content .container"),
-				select = $('#dir'),
-				section = mainContent.find('.sections .section'),
-				sectionTemplate = $(`<div class="section"><pre></pre><div class="table-wrap"><table class="table table-condensed section-table"><thead><tr><td>VARIABLE NAME</td><td>EN</td><td>RU</td></tr></thead><tbody></tbody></table></div`);
+		select = $("#dir"),
+		section = mainContent.find(".sections .section"),
+		sectionTemplate = $(
+			`<div class="section"><pre></pre><div class="table-wrap"><table class="table table-condensed section-table"><thead><tr><td>VARIABLE NAME</td><td>EN</td><td>RU</td></tr></thead><tbody></tbody></table></div`
+		);
 	let count = 0;
+
+	/*		Get Sections List		*/
 
 	const getSectionsList = $.getJSON("getlist.json", response => {
 		let options = [];
 
 		$.each(response, (i, element) => {
-
-			options.push($(`<option value=${element}>${element}</option>`))
+			options.push($(`<option value=${element}>${element}</option>`));
 			select.append(options);
 		});
 
-		select.on('change', () => {
-			let dirIndex = $(this).find(':selected').val();
+		select.on("change", () => {
+			let dirIndex = $(this).find(":selected").val();
 
-			$('.loader-container').addClass('hidden');
-			$('.main-content .container > *').addClass('hidden');
+			$(".loader-container").addClass("hidden");
+			$(".main-content .container > *").addClass("hidden");
 
 			const requestContent = $.getJSON(`${dirIndex}.json`, response => {
 				response.map(files => {
 					renderContent(files);
 				});
-			}).fail(error => { console.log( "error at request content" ) });
-			
-			const renderContent = files => {
+			}).fail(error => {
+				console.log("error at request content");
+			});
 
-				$.each(files, (filename,obj) => {
-					let filesDiv = $('.hidden_template').clone();
-						
-					filesDiv
-						.attr('id',`${count++}`)
-						.removeClass('hidden_template')
-						.css('display','flex')
-						.appendTo(mainContent);
-
-					filesDiv.find('.file > pre')
-						.text(filename)
-						.append($('<i class="fa fa-sort-down"></i>'));
-
-					$.each(files[filename],(groupname,obj) => {
-						let sectionClone = sectionTemplate.clone();
-
-						sectionClone.find('pre')
-							.append(groupname)
-							.append($('<i class="fa fa-sort-down"></i>'))
-
-						obj.map((val) => {
-							let tr = $('<tr></tr>'),
-									placeholder = '';
-
-							if (val.ru === '') {
-								placeholder = 'Введите перевод';
-							} else {
-								placeholder = val.ru;
-							}
-
-							tr.append(`<td><span>${val.var}</span></td>`);
-							tr.append(`<td><span>${val.en}</span></td>`);
-							tr.append(`<td><span>${(val.ru === '') ? "Не указано": val.ru}</span><input type="text" class="td-edit hidden invisible"></input></td>`);
-							tr.find('td').eq(2).find('.td-edit').attr('placeholder', placeholder)
-							tr.appendTo(sectionClone.find('tbody'));
-						});
-
-						sectionClone.appendTo(filesDiv.find('.sections'));
-					});
-				});
-			};			
 		});
 	})
 		.done(() => {
 			select.selectize({});
 		})
-		.fail(() => { error("error at request sections list") });
-		
+		.fail(() => {
+			error("error at request sections list");
+		});
+
+	const renderContent = files => {
+		$.each(files, (filename, obj) => {
+			let filesDiv = $(".hidden_template").clone();
+
+			filesDiv
+				.attr("id", `${count++}`)
+				.removeClass("hidden_template")
+				.css("display", "flex")
+				.appendTo(mainContent);
+
+			filesDiv
+				.find(".file > pre")
+				.text(filename)
+				.append($('<i class="fa fa-sort-down"></i>'));
+
+			$.each(files[filename], (groupname, obj) => {
+				let sectionClone = sectionTemplate.clone();
+
+				sectionClone
+					.find("pre")
+					.append(groupname)
+					.append($('<i class="fa fa-sort-down"></i>'));
+
+				obj.map(val => {
+					let tr = $("<tr></tr>"), placeholder = "";
+
+					if (val.ru === "") {
+						placeholder = "Введите перевод";
+					} else {
+						placeholder = val.ru;
+					}
+
+					tr.append(`<td><span>${val.var}</span></td>`);
+					tr.append(`<td><span>${val.en}</span></td>`);
+					tr.append(
+						`<td><span>${val.ru === "" ? "Не указано" : val.ru}</span><input type="text" class="td-edit hidden invisible"></input></td>`
+					);
+					tr
+						.find("td")
+						.eq(2)
+						.find(".td-edit")
+						.attr("placeholder", placeholder);
+					tr.appendTo(sectionClone.find("tbody"));
+				});
+
+				sectionClone.appendTo(filesDiv.find(".sections"));
+			});
+		});
+	};
 	const error = msg => {
 		let div = $('<div class="content-error"></div>'),
-				errorText = $(`<h1>${msg}</h1>`);
+			errorText = $(`<h1>${msg}</h1>`);
 
-		div
-			.append(errorText)
-			.addClass('blink');
+		div.append(errorText).addClass("blink");
 
-		mainContent
-			.html('')
-			.append(div);
+		mainContent.html("").append(div);
 	};
 
+	/*		Events		*/
 
-
-			/* Events */
-
-	mainContent.on("click", ".file pre",event => {
-		$(event.target).parent('.file').children('.sections').slideToggle(400);
-		$(event.target).find('.fa').toggleClass('fa-sort-up');
+	mainContent.on("click", ".file pre", event => {
+		$(event.target).parent(".file").children(".sections").slideToggle(400);
+		$(event.target).find(".fa").toggleClass("fa-sort-up");
 	});
 
 	mainContent.on("click", ".section pre", event => {
@@ -104,12 +111,11 @@ $(function() {
 	});
 
 	mainContent.on("click", "tbody td span", event => {
-		$(event.target)
-			.toggleClass("invisible")
-			.fadeToggle()
-			.toggleClass("hidden");
+		$(event.target).toggleClass("invisible").fadeToggle().toggleClass("hidden");
 
-		$(event.target).parent().find("input.td-edit")
+		$(event.target)
+			.parent()
+			.find("input.td-edit")
 			.fadeToggle()
 			.toggleClass("hidden invisible active")
 			.focus();
@@ -118,7 +124,9 @@ $(function() {
 	mainContent.on("keydown", "tbody .td-edit", event => {
 		switch (event.which) {
 			case 13: //
-				$(event.target).parent('td').find("span")
+				$(event.target)
+					.parent("td")
+					.find("span")
 					.fadeToggle()
 					.toggleClass("hidden invisible");
 
@@ -127,8 +135,8 @@ $(function() {
 					.fadeToggle()
 					.toggleClass("hidden active");
 
-				if (!(($(event.target).val()) === "")) {
-					$(event.target).parent('td').find("span").text($(event.target).val());
+				if (!($(event.target).val() === "")) {
+					$(event.target).parent("td").find("span").text($(event.target).val());
 				}
 
 				$(event.target).val("");
@@ -136,7 +144,8 @@ $(function() {
 				break;
 			case 27: // esc
 				$(event.target)
-					.parent().find("span")
+					.parent()
+					.find("span")
 					.fadeToggle()
 					.toggleClass("hidden invisible");
 
@@ -152,20 +161,18 @@ $(function() {
 
 	$(window).scroll(() => {
 		if ($(this).scrollTop() > 1200) {
-			$('.top').addClass('active');
+			$(".top").addClass("active");
 		} else {
-			$('.top').removeClass('active');
+			$(".top").removeClass("active");
 		}
 	});
 
-	$('.top').on('click', () => {
-		$('htmlm, body')
-			.stop()
-			.animate({scrollTop: 0}, 'slow', 'swing');
+	$(".top").on("click", () => {
+		$("htmlm, body").stop().animate({ scrollTop: 0 }, "slow", "swing");
 	});
 });
 
-$(window).on('load', () => {
-	$('.preloader').delay(100).fadeOut('slow');
-	$('.loader-container').delay(5000).removeClass('hidden');
+$(window).on("load", () => {
+	$(".preloader").delay(100).fadeOut("slow");
+	$(".loader-container").delay(5000).removeClass("hidden");
 });
